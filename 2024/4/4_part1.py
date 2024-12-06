d48a60af69006f434caf76f2a525bc5eee38c663
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-12-06 12:24:59 +0200

    Fix coords in day4 part1

--- 2024/4/4_part1.py
+++ 2024/4/4_part1.py
@@ -2,9 +2,9 @@ with open("4.txt", "r") as f:
   grid = f.read().splitlines()
 
 res = 0
-coords = [(-1, 1), (0, 1), (1, 1),
-         (-1, 0),           (1, 0),
-         (-1, -1), (0, -1), (1, -1)]
+coords = [(-1, -1), (-1, 0), (1, -1),
+         (0, -1),           (0, 1),
+         (-1, 1), (1, 0), (1, 1)]
 
 def is_valid(r, c):
   return 0 <= _r < len(grid) and 0 <= _c < len(grid[0])
@@ -17,8 +17,8 @@ for r in range(len(grid)):
         if is_valid(_r, _c) and grid[_r][_c] == "M":
           _r, _c = _r + coord[0], _c + coord[1]
           if is_valid(_r, _c) and grid[_r][_c] == "A":
-            _r, _c = _r + coord[0], _c + coord[1] 
+            _r, _c = _r + coord[0], _c + coord[1]
             if is_valid(_r, _c) and grid[_r][_c] == "S":
-              res += 1 
+              res += 1
 
 print(res)
